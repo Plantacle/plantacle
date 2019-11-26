5209532e05155fddccdeb94d91e10497b0f49e56
Hayen Phan <hayenphan@outlook.com>
2019-11-26 17:44:41 +0100

    Styled measurement circles

--- src/components/atoms/Measurement.js
+++ src/components/atoms/Measurement.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import styled from 'styled-components';
+import Methane from '../svg/Methane';
+import Moist from '../svg/Moist';
+import Temperature from '../svg/Temperature';
 
 const Circle = styled.div`
-  height: 50px;
-  width: 50px;
+  height: 32px;
+  width: 32px;
   border-radius: 50%;
   border-color: #324BB8;
   border-style: solid;
   border-width: 2px;
+  margin-top: 10px;
 `;
 
-export const Measurement = () => (
-  <Circle>
+class Measurement extends React.Component {
+  render() {
+    const name = this.props.name;
 
-  </Circle>
-)
+     if (name === "methane") {
+       return (
+         <div>
+         <Circle> <Methane width="20" height="20" /> </Circle>
+         </div>
+       )
+     } else if (name === "moist") {
+       return (
+         <div>
+         <Circle> <Moist width="20" height="20" /> </Circle>
+         </div>
+       )
+     } else if (name === "temperature") {
+      return (
+        <div>
+        <Circle> <Temperature width="20" height="20" /> </Circle>
+        </div>
+      )
+    }
+
+    return <div></div>;
+
+  }
+}
 
 export default Measurement;
--- src/pages/Styleguide1.js
+++ src/pages/Styleguide1.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Measurement from '../components/atoms/Measurement';
 
-export const Styleguide1 = () => (
-    <Measurement> </Measurement>
-)
+
+class Styleguide1 extends React.Component {
+  render() {
+    return (
+      <div>
+
+        <Measurement name="temperature"> </Measurement>
+        <Measurement name="methane"> </Measurement>
+        <Measurement name="moist"> </Measurement>
+
+      </div>
+    )
+  }
+}
+
 
 export default Styleguide1;
