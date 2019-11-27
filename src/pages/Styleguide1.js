e38dd1c2041786dc275e0bdca0e3a63293bc6722
Hayen Phan <hayenphan@outlook.com>
2019-11-27 17:59:07 +0100

    Styleguide fix

--- src/pages/Styleguide1.js
+++ src/pages/Styleguide1.js
@@ -6,11 +6,28 @@ class Styleguide1 extends React.Component {
   render() {
     return (
       <div>
+          <div className="measurements">
 
-        <Measurement name="temperature" value="66°c"> </Measurement>
-        <Measurement name="methane" value="5,42"> </Measurement>
-        <Measurement name="moist"  value="15 %"> </Measurement>
+            <Measurement name="temperature" value="66°c"> </Measurement>
+            <Measurement name="methane" value="5,42"> </Measurement>
+            <Measurement name="moist"  value="15 %"> </Measurement>
 
+          </div>
+
+
+          <br />
+          <br />
+          <br />
+
+
+
+          <div className="measurements">
+
+            <Measurement name="temperature" value="66°c"> </Measurement>
+            <Measurement name="methane" value="5,42"> </Measurement>
+            <Measurement name="moist"  value="15 %"> </Measurement>
+
+          </div>
       </div>
     )
   }
