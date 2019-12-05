651b79e6c0626f2cb245b28406e19db7a273a0e6
Hayen Phan <hayenphan@outlook.com>
2019-12-05 21:10:31 +0100

    Changed overview path

--- src/components/App.js
+++ src/components/App.js
@@ -26,8 +26,9 @@ function App() {
         <Router>
           <Switch>
 
-            <Route exact path="/" component={Overview} />
+            //<Route exact path="/" component={Overview} />
             <Route path="/tasks" component={Tasks} />
+            <Route exact path="/overview" component={Overview} />
 
             //
             <Route path="/styleguide1" component={Styleguide1} />
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import Measurements from '../components/molecules/Measurements';
+import CurrentDay from '../components/atoms/CurrentDay';
+import CompostTransition from '../components/atoms/CompostTransition';
+import OverviewButton from '../components/atoms/OverviewButton';
 
-export const Overview = () => (
-  <div>
-    <p> This is the overview page </p>
-  </div>
-)
+
+class Overview extends React.Component {
+  render() {
+    return (
+      <div>
+          <div className="measurements">
+
+            <Measurements />
+
+          </div>
+
+          <br />
+          <br />
+          <br />
+
+          <div className="date">
+
+            <CurrentDay />
+
+          </div>
+
+          <br />
+          <br />
+          <br />
+
+          <div className="transition">
+            <CompostTransition />
+          </div>
+
+
+          <br />
+
+          <div className="button">
+            <OverviewButton />
+          </div>
+
+
+      </div>
+    )
+  }
+}
+
 
 export default Overview;
