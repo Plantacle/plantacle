3b5067c4f32e647aeba7a68092412ca43cd960b3
Hayen Phan <hayenphan@outlook.com>
2020-01-06 19:07:59 +0100

    Combining react-bootstrap with styled components

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -3,48 +3,39 @@ import Measurements from '../components/molecules/Measurements';
 import CurrentDay from '../components/atoms/CurrentDay';
 import CompostTransition from '../components/atoms/CompostTransition';
 import OverviewButton from '../components/atoms/OverviewButton';
-
+import styled from 'styled-components';
 
 import {Bootstrap, Grid, Row, Col, Container} from 'react-bootstrap';
 
+const StyledContainer = styled(Container)`
+    width: 100px;
+    height: 100px;
+    background-color: yellow;
+`;
 
 class Overview extends React.Component {
   render() {
     return (
-      <Container>
-          <Row>
-
-            <Measurements />
-
-          </Row>
-
-          <br />
-          <br />
-          <br />
+      <StyledContainer>
 
           <Row>
 
-            <CurrentDay />
+              <Measurements />
+              <CurrentDay />
 
           </Row>
-
-          <br />
-          <br />
-          <br />
 
           <Row>
-            <CompostTransition />
+              <CompostTransition />
           </Row>
 
-
-          <br />
 
           <Row>
             <OverviewButton />
           </Row>
 
 
-      </Container>
+      </StyledContainer>
     )
   }
 }
