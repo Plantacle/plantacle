639c513c2c547fd3f844d3228a480f206b3388fb
Hayen Phan <hayenphan@outlook.com>
2020-01-07 15:21:56 +0100

    Positioned elements Overview

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -8,35 +8,68 @@ import styled from 'styled-components';
 import {Bootstrap, Grid, Row, Col, Container} from 'react-bootstrap';
 
 const StyledContainer = styled(Container)`
-    && {
-      padding-left: 25px;
-      padding-right: 25px;
-    }
+  && {
+    padding-left: 25px;
+    padding-right: 25px;
+    padding-top: 25px;
+  }
 `;
 
+const FirstRow = styled(Row)`
+  && {
+    position: relative;
+  }
+`;
+
+const SecondRow = styled(Row)`
+  && {
+    justify-content: center;
+  }
+`;
+
+const ThirdRow = styled(Row)`
+  && {
+    justify-content: center;
+  }
+`;
+
+const SecondCol = styled(Col)`
+  && {
+    position: absolute;
+    top: 30%;  /* position the top  edge of the element at the middle of the parent */
+    left: 82%; /* position the left edge of the element at the middle of the parent */
+    transform: translate(-50%, -50%); /* This is a shorthand of
+                                       translateX(-50%) and translateY(-50%) */
+  }
+`;
+
+const CompostTransitionWrapper = styled.div`
+  margin-bottom: 30px;
+`;
 
 class Overview extends React.Component {
   render() {
     return (
-      <StyledContainer>
+        <StyledContainer>
+            <FirstRow>
+                <Col>
+                    <Measurements />
+                </Col>
 
-          <Row>
+                <SecondCol>
+                    <CurrentDay />
+                </SecondCol>
+            </FirstRow>
 
-              <Measurements />
-              <CurrentDay />
+          <SecondRow>
+              <CompostTransitionWrapper>
+                  <CompostTransition />
+              </CompostTransitionWrapper>
+          </SecondRow>
 
-          </Row>
-
-          <Row>
-              <CompostTransition />
-          </Row>
-
-
-          <Row>
-            <OverviewButton />
-          </Row>
-
-
+          <ThirdRow>
+              <OverviewButton />
+          </ThirdRow>
       </StyledContainer>
     )
   }
