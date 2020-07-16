fc661240bba1eb166b009e4d71e4e06622e098d8
Hayen Phan <hayenphan@MacBook-Pro-van-Hayen.local>
2020-07-17 01:28:35 +0200

    WIP

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -22,48 +22,41 @@ class Profile extends React.Component {
     }
 
     getUserInfo = () => {
-        // const token = localStorage.getItem("decoded_token");
-        // const decodedToken = JSON.parse(token);
+        const token = localStorage.getItem("decoded_token");
+        const decodedToken = JSON.parse(token);
+
+        const firstName = decodedToken.firstName;
+        const lastName = decodedToken.lastName;
+        const compostPoints = decodedToken.compostPoints;
+        const totalActivity = decodedToken.totalActivity;
+
+        this.setState({
+          firstName: firstName, lastName: lastName, compostPoints: compostPoints, totalActivity: totalActivity
+        })
+
+
+        // DEZE HEB JE NODIG VOOR TASKS
+
+        // let decodedToken = JSON.parse(localStorage.getItem("decoded_token"));
+        // const token = decodedToken.id;
         //
-        // const firstName = decodedToken.firstName;
-        // const lastName = decodedToken.lastName;
-        // const compostPoints = decodedToken.compostPoints;
-        // const totalActivity = decodedToken.totalActivity;
+        // const config = {
+        //     headers: { Authorization: `Bearer ${token}` }
+        // };
         //
-        // this.setState({
-        //   firstName: firstName, lastName: lastName, compostPoints: compostPoints, totalActivity: totalActivity
+        // axios({
+        // method: 'get',
+        // url: 'http://localhost:4000/users',
+        // config
         // })
-
-        let decodedToken = JSON.parse(localStorage.getItem("decoded_token"));
-        const token = decodedToken.id;
-
-        // const api = `your api here`
-        // axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} })
-        // .then(res => {
-        //     console.log(res.data);
-        // this.setState({
-        //     items: res.data,  /*set response data in items array*/
-        //     isLoaded : true,
-        //     redirectToReferrer: false
+        // .then(response => {
+        //
+        //   console.log(response.data);
+        //
         // })
-
-        const config = {
-            headers: { Authorization: `Bearer ${token}` }
-        };
-
-        axios({
-        method: 'get',
-        url: 'http://localhost:4000/users',
-        config
-        })
-        .then(response => {
-
-          console.log(response.data);
-
-        })
-        .catch( err => {
-          console.log(err)
-        })
+        // .catch( err => {
+        //   console.log(err)
+        // })
 
 
     }
@@ -73,6 +66,11 @@ class Profile extends React.Component {
     render() {
       return (
           <Container>
+
+          <ul>
+          <li>{this.state.firstName}</li>
+          <li>{this.state.compostPoints}</li>
+          </ul>
 
           </Container>
       )
